79da48b259fb21efd580ad415e0e7c20372c650b
Juha Halmu <juha.halmu@iki.fi>
2025-01-27 12:43:49 +0200

    Intial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/jhalmu/go-page
+
+go 1.23.5
+
+require github.com/a-h/templ v0.3.819
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/jhalmu/go-page/internal/components"
+	"log/slog"
+	"net/http"
+	"os"
+)
+
+func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /{$}", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/html; charaset utf-8")
+		components.Home("Lucky me!").Render(r.Context())
+	})
+
+	srv := http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	logger.Info("Server listening on port :8080")
+
+	srv.ListenAndServe()
+
+}
